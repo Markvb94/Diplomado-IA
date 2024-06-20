21dabaea4f725156bd43556e598d9f12da90e370
Marco Velazquez <markovelazquez13@gmail.com>
2024-06-19 22:23:53 -0600

    Ejercicio while1

---
+++ Modulo_1/3.1_Operadores_ejercicios.py
@@ -0,0 +1,23 @@
+"""
+Escribir un programa que solicite 2 numeros y luego imprima.
+1. Suma
+2. Resta
+3. Producto
+4.El cubo de la suma de los 2 numeros
+5. El cociente de la división del primer numero por el segundo
+"""
+
+Num1 = float(input("Ingresa el primer numero"))
+Num2 = float(input("Ingresa el segundo numero"))
+Suma = Num1 + Num2
+Resta = Num1 - Num2
+Producto = Num1 + Num2
+Cubo = Suma**3
+Cociente = Num1/Num2
+
+print ("La suma de los numeros es:", Suma)
+print ("La resta de los numeros es:", Resta)
+print ("El producto de los numeros es:", Producto)
+print ("El cubo de los numeros es:", Cubo)
+print ("Cociente de la division de los numeros:", Cociente)
+ 
